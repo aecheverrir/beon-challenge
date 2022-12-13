452c6bbf45aa315bd8d760ae008efa0d0ad46321
Alejandro <aecheverri@Linas-MacBook-Air.local>
2022-12-12 22:53:19 -0500

    initial commit

---
+++  README.md
@@ -0,0 +1,11 @@
+# Requirements
+- Node
+
+## How to make it work 
+1. npm i
+2. npx cypress
+3. run the spec
+
+## Cypress
+Test files are under cypress/e2e/search folder.
+Custome commands created are under cypress/support/commands.js
---
+++ cypress/e2e/search/basicSearch.cy.js
@@ -0,0 +1,15 @@
+describe('Tests for a basic google search', () => {
+    beforeEach(() => {
+      cy.visit('https://google.com')
+    })
+
+    it(`performing a search on the main page`, () => {
+        cy.googleSearch('ABCDEFG');
+        cy.get('#rso').children().eq(5).find('[data-header-feature="0"]').find('a').first().as('third_result');
+        cy.get('@third_result').invoke('attr', 'href').as('href_value');
+        cy.get('@third_result').click();
+        cy.get('@href_value').then(href => {
+            
+        });
+    })
+})
